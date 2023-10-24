0bfcce7bcbba57ff57c027c6ab3fb920b2fcd21b
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-10-24 14:32:30 +0200

    Added framer motion to app component and redid big image texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { motion } from "framer-motion"
 import { Chart as ChartJS, ArcElement, Tooltip, Title, Legend, CategoryScale, LinearScale, BarElement, LineElement, PointElement, } from "chart.js";
 ChartJS.register(ArcElement, Tooltip, Title, Legend, CategoryScale, LinearScale, BarElement, PointElement,
   LineElement,);
@@ -15,40 +16,65 @@ const moviesByDurationLineConfig = getMoviesByDurationLineConfig()
 const moviesByGenrePieConfig = getMoviesByGenrePieConfig()
 
 function App() {
-
-  const options = () => {
-    return {
+  // For line chart
+  const options = {
       scales: {
         x: { 
           display: false // Gömmer labels i X led
         }
       }
-    }
   }
+
+  // Framer motion
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 1.0,
+        staggerChildren: 1.5
+      }
+    }
+  };
+  
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
 
   return (
     <main>
       <Navbar />
       <div className="big-image">
-        <h1 className="rotate-title">MovieStats</h1>
-        <h2 className="squish-squash-text">En tjänst till MovieFlex</h2>
+        <div className="border-titles">
+          <h1 className="rotate-title">MovieStats</h1>
+          <h2 className="squish-squash-text">En tjänst till MovieFlex</h2>
+        </div>
       </div>
-      <div className="card">
-        <h1 className="title">Antal filmer i olika språk</h1>
-        <Pie className="chart" id="movies-by-language" data={moviesByLanguagePieConfig} />
-      </div>
-      <div className="card">
-        <h1 className="title">Antal olika sorts filmer per månad</h1>
-        <Bar className="chart" id="movie-releases-by-month" data={moviesByMonthBarsConfig} />
-      </div>
-      <div className="card">
-        <h1 className="title">Alla olika filmers längd</h1>
-        <Line className="chart" id="movie-durations" data={moviesByDurationLineConfig} options={options()} />
-      </div>
-      <div className="card">
-        <h1 className="title">Alla genrer av filmer</h1>
-        <Pie className="chart" id="movies-by-genre" data={moviesByGenrePieConfig} />
-      </div>
+      <motion.div variants={container} initial="hidden"
+    animate="visible">
+        <motion.div className="card" variants={item}>
+          <h1 className="title">Antal filmer i olika språk</h1>
+          <Pie className="chart" id="movies-by-language" data={moviesByLanguagePieConfig} />
+        </motion.div>
+        <motion.div className="card" variants={item}>
+          <h1 className="title">Antal olika sorts filmer per månad</h1>
+          <Bar className="chart" id="movie-releases-by-month" data={moviesByMonthBarsConfig} />
+        </motion.div>
+        <motion.div className="card" variants={item}>
+          <h1 className="title">Alla olika filmers längd</h1>
+          <Line className="chart" id="movie-durations" data={moviesByDurationLineConfig} options={options} />
+        </motion.div>
+        <motion.div className="card" variants={item}>
+          <h1 className="title">Alla genrer av filmer</h1>
+          <Pie className="chart" id="movies-by-genre" data={moviesByGenrePieConfig} />
+        </motion.div>
+      </motion.div>
     </main>
   )
 }
